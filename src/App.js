f09b1a721a780c447d752e4ba73ca4ae503b4e31
send-2-irfan <irfanullah.bses19@iba-suk.edu.pk>
2023-04-13 23:54:45 +0500

    done with tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,44 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
-const App = () => {
-  const [inputString, setInputString] = useState("");
-  const [selectedCFG, setSelectedCFG] = useState(null);
-  const [derivationSteps, setDerivationSteps] = useState([]);
-
-  const cfgs = {
-    // define CFGs here
-  };
-
-  const handleInputChange = (e) => {
-    setInputString(e.target.value);
-  };
-
-  const handleCFGSelect = (e) => {
-    setSelectedCFG(e.target.value);
-  };
-
-  const handleDeriveClick = () => {
-    // implement the CFG derivation logic here
-    // update the derivationSteps state with each substitution step and the resulting string
-  };
-
+function App() {
   return (
     <div>
-      <label htmlFor="inputString">Input String:</label>
-      <input
-        type="text"
-        id="inputString"
-        value={inputString}
-        onChange={handleInputChange}
-      />
-      <label htmlFor="cfgSelect">Select CFG:</label>
-      <select id="cfgSelect" value={selectedCFG} onChange={handleCFGSelect}>
-        {/* render dropdown options based on the available CFGs */}
-      </select>
-      <button onClick={handleDeriveClick}>Derive</button>
-      {/* render the derivation steps and the resulting string */}
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
-};
+}
 
 export default App;

170fca5869f29181b14b0a09c67e12e55c77076c
send-2-irfan <irfanullah.bses19@iba-suk.edu.pk>
2023-04-18 00:28:10 +0500

    navbar css is almost done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,8 +10,8 @@ import {
 
 function NavBar() {
   return (
-    <div>
-      <ul>
+    <div className="w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-gray-700/80">
+      <ul className="hidden sm:flex px-4">
         <li>
           <a href="/">Home</a>
         </li>
@@ -25,8 +25,30 @@ function NavBar() {
           <a href="#contact">Contact</a>
         </li>
       </ul>
-      <div>
-        <FaFacebookF />
+      <div className="flex justify-between">
+        <FaFacebookF className="mx-4" />
+        <FaTwitter className="mx-4" />
+        <FaGooglePlusG className="mx-4" />
+        <FaInstagram className="mx-4" />
+      </div>
+      <div className="sm:hidden z-10">
+        <FaBars size={20} className="mr-4 cursor-pointer" />
+      </div>
+      <div className="overflow-y-hidden md:hidden ease-in duration-300 absolute text-gray-300 left-0 top-0 w-full h-screen bg-black/90  px-4 py-7 flex-col">
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="#galary">Galary</a>
+          </li>
+          <li>
+            <a href="#deals">Deals</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
       </div>
     </div>
   );

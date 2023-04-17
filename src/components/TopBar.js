afc672ac428a1a594d9bbf2797c6c8ba4b9edcd3
send-2-irfan <irfanullah.bses19@iba-suk.edu.pk>
2023-04-17 23:47:38 +0500

    TopBar file tailwind css added

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,19 +4,25 @@ import { BsChatSquareDots } from "react-icons/bs";
 
 function TopBar() {
   return (
-    <div>
-      <div>
-        <BsChatSquareDots />
-        <h1>WEEKAWAY</h1>
+    <div className="flex justify-between items-center px-4 py-2">
+      <div className="flex items-center">
+        <BsChatSquareDots
+          size={30}
+          className="text-[var(--primary-dark)] mr-2"
+        />
+        <h1 className="text-xl font-bold text-gray-700">WEEKAWAY</h1>
       </div>
-      <div>
-        <div>
-          <AiOutlineClockCircle />
-          <h1>9:00 AM - 5:00 PM</h1>
+      <div className="flex">
+        <div className="hidden md:flex items-center px-6">
+          <AiOutlineClockCircle
+            size={20}
+            className="mr-2 text-[var(--primary-dark)]"
+          />
+          <h1 className="text-sm text-gray-700">9:00 AM - 5:00 PM</h1>
         </div>
-        <div>
-          <AiFillPhone />
-          <h1>+254 705-444-1234</h1>
+        <div className="hidden md:flex items-center px-6">
+          <AiFillPhone className="mr-2 text-[var(--primary-dark)]" />
+          <h1 className="text-sm text-gray-700">+254 705-444-1234</h1>
         </div>
         <button>Get a free Quote</button>
       </div>

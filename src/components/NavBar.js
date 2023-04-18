02ccc5fbdac3c9c20a75b5c83899595fe7920441
send-2-irfan <irfanullah.bses19@iba-suk.edu.pk>
2023-04-18 06:18:01 +0500

    conditional css applied

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsChatSquareDots } from "react-icons/bs";
 import {
   FaBars,
@@ -9,6 +9,11 @@ import {
 } from "react-icons/fa";
 
 function NavBar() {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
   return (
     <div className="w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-gray-700/80">
       <ul className="hidden sm:flex px-4">
@@ -31,21 +36,27 @@ function NavBar() {
         <FaGooglePlusG className="mx-4" />
         <FaInstagram className="mx-4" />
       </div>
-      <div className="sm:hidden z-10">
+      <div onClick={handleNav} className="sm:hidden z-10">
         <FaBars size={20} className="mr-4 cursor-pointer" />
       </div>
-      <div className="overflow-y-hidden md:hidden ease-in duration-300 absolute text-gray-300 left-0 top-0 w-full h-screen bg-black/90  px-4 py-7 flex-col">
-        <ul>
-          <li>
+      <div
+        className={
+          nav
+            ? "overflow-y-hidden md:hidden ease-in duration-300 absolute text-gray-300 left-0 top-0 w-full h-screen bg-black/90  px-4 py-7 flex-col"
+            : "absolute top-0 h-screen left-[-100%] ease-in duration-500"
+        }
+      >
+        <ul className="h-full w-full text-center pt-12">
+          <li className="text-2xl py-8">
             <a href="/">Home</a>
           </li>
-          <li>
+          <li className="text-2xl py-8">
             <a href="#galary">Galary</a>
           </li>
-          <li>
+          <li className="text-2xl py-8">
             <a href="#deals">Deals</a>
           </li>
-          <li>
+          <li className="text-2xl py-8">
             <a href="#contact">Contact</a>
           </li>
         </ul>

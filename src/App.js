6e7fd9da336f34407b9e7a5c72d57bfbe59c2006
send-2-irfan <irfanullah.bses19@iba-suk.edu.pk>
2023-04-18 10:52:56 +0500

    Done with Footer component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Activites from "./components/Activites";
 import Booking from "./components/Booking";
 import Galary from "./components/Galary";
 import Footer from "./components/Footer";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Activites />
       <Booking />
       <Galary />
+      <Contact />
       <Footer />
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { BsChatSquareDots } from "react-icons/bs";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="max-w-[1140px] w-full m-auto border-t-4 py-8">
+      <div className="flex items-center justify-center m-auto">
+        <BsChatSquareDots
+          size={30}
+          className="text-[var(--primary-dark)] mr-2"
+        />
+        <h1 className="text-xl font-bold text-gray-700">WEEKAWAY</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
